ef086b435ff078b5bacac77986018f900f07fb8a
Filip Vanden Eynde <filipve1994@gmail.com>
2021-03-10 16:45:02 +0100

    update

---
+++ blog/welcome.md
@@ -0,0 +1,12 @@
+---
+title: "Welcome"
+date: 2020-07-10
+---
+
+# Welcome
+
+Welcome to my blog!
+
+This is the first post in this brand new blog.
+Of course we have all the power of Markdown here,
+so we can make things **bold** or _italic_.
--- gridsome.config.js
+++ gridsome.config.js
@@ -5,45 +5,95 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
+    siteName: 'Gridsome Blog Starter',
 
-  plugins: [
-    {
-      use: '@gridsome/source-filesystem',
-      options: {
-        // path: 'posts/**/*.md',
-        path: 'blog/**/*.md',
-        typeName: 'Post',
-        refs: {
-          tags: {
-            typeName: 'Tag',
-            create: true
-          }
+    plugins: [
+        {
+            use: 'gridsome-plugin-tailwindcss',
         },
-        remark: {
-          plugins: [
-            // ...local plugins
-          ]
-        }
-      }
-    },
-    {
-      use: `gridsome-plugin-netlify-cms`,
-      options: {
-        publicPath: `/admin`,
-        modulePath: `src/admin/index.js`
-      }
-    },
-  ],
+        {
+            use: '@gridsome/vue-remark',
+            options: {
+                typeName: 'Documentation', // Required
+                baseDir: './docs', // Where .md files are located
+                pathPrefix: '/docs', // Add route prefix. Optional
+                template: './src/templates/Documentation.vue', // Optional
+                plugins: [
+                    ['gridsome-plugin-remark-shiki', {theme: 'Material-Theme-Palenight', skipInline: true}]
+                ],
+            }
+        },
+        {
+            use: '@gridsome/source-filesystem',
+            options: {
+                // path: 'posts/**/*.md',
+                path: 'blog/**/*.md',
+                typeName: 'Post',
+                refs: {
+                    tags: {
+                        typeName: 'Tag',
+                        create: true
+                    }
+                },
+                remark: {
+                    plugins: [
+                        // ...local plugins
+                    ]
+                }
+            }
+        },
+        {
+            use: `gridsome-plugin-netlify-cms`,
+            options: {
+                publicPath: `/admin`,
+                modulePath: `src/admin/index.js`
+            }
+        },
 
-  transformers: {
-    remark: {
-      externalLinksTarget: '_blank',
-      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
-      anchorClassName: 'icon icon-link',
-      plugins: [
-        // ...global plugins
-      ]
-    }
-  },
+        {
+            use: 'gridsome-plugin-rss',
+            options: {
+                contentTypeName: 'Post',
+                feedOptions: {
+                    title: 'Gridsome Starter Blog',
+                    feed_url: 'https://gridsome-blog-filip.netlify.app/rss.xml',
+                    site_url: 'https://gridsome-blog-filip.netlify.app/'
+                },
+                feedItemOptions: node => ({
+                    title: node.title,
+                    description: node.summary,
+                    url: 'https://gridsome-blog-filip.netlify.app' + node.path,
+                    author: 'Filip Vanden Eynde',
+                    date: node.date
+                }),
+                output: {
+                    dir: './static',
+                    name: 'rss.xml'
+                }
+            }
+        },
+        {
+            use: '@gridsome/plugin-sitemap',
+            options: {
+                cacheTime: 600000, // default
+            }
+        },
+    ],
+
+    templates: {
+        Tag: '/tag/:id',
+        Post: "/blog/post/:title",
+    },
+
+    transformers: {
+        remark: {
+            externalLinksTarget: '_blank',
+            externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+            anchorClassName: 'icon icon-link',
+            plugins: [
+                // ...global plugins
+                [ 'gridsome-plugin-remark-shiki', { theme: 'Material-Theme-Palenight', skipInline: true } ]
+            ]
+        }
+    },
 }
